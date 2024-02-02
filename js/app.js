601faf71169cefa7b077e8bef063b07b0b0d91cf
k8mcelhnaey <kqmcelhaney@gmail.com>
2024-02-02 13:15:20 -0700

    Psuedo Code

---
+++ js/app.js
@@ -0,0 +1,36 @@
+console.log("Tayo-na!-Let's play!")
+/*----- constants -----*/
+//const cards
+//let hasFlippedCard 
+//let cards [12 card array]
+
+
+
+/*----- app's state (variables) -----*/
+//let firstCard, secondCard...
+//let matchesMade
+//let movesUsed
+//let movesRemaining (48-movesUsed)
+//letWin
+//letLose
+
+/*----- cached element references -----*/
+//firstCardPicked
+//secondCardPicked
+
+
+/*----- event listeners -----*/
+//playGame
+//flipCard
+//easyTimer
+//mediumTimer
+//hardTimer
+//resetGame
+
+/*----- functions -----*/
+//flipCard
+//disableCard (when matched)
+//checkForMatch
+//unflipCard (when two unmacthed cards are flipped)
+//celebrateWinner (when all 12 pairs matched)
+//shuffleCards (activated at play game and reset)
