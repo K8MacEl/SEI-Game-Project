1fa653cc56537879f62e37ad589fb25761460cac
k8mcelhnaey <kqmcelhaney@gmail.com>
2024-02-02 17:33:03 -0700

    HTML Divs Completed

--- js/app.js
+++ js/app.js
@@ -1,30 +1,88 @@
 console.log("Tayo-na!-Let's play!")
 /*----- constants -----*/
-//const cards
-//let hasFlippedCard 
-//let cards [12 card array]
+const cardArray = [
+    {
+        name: 'jeepney 1',
+        img: 
+    },
+    name: 'jeepney 2',
+    img:
+    {
+        name: 'jeepney 3',
+        img:
+    },
 
+    {
+        name: 'jeepney 4',
+        img:
+    },
 
+    {
+        name: 'jeepney 5',
+        img:
+    },
+
+    {
+        name: 'jeepney 6',
+        img:
+    },
+
+    {
+        name: 'jeepney 7',
+        img:
+
+    },
+
+    {
+        name: 'jeepney 8',
+        img:
+
+    },
+
+    {
+        name: 'jeepney 9',
+        img:
+
+    },
+
+    {
+        name: 'jeepney 10',
+        img:
+
+    },
+
+    {
+        name: 'jeepney 11',
+        img:
+    },
+
+    {
+        name: 'jeepney 12',
+        img:
+    },
+
+]
 
 /*----- app's state (variables) -----*/
+
+//let hasFlippedCard 
 //let firstCard, secondCard...
 //let matchesMade
 //let movesUsed
 //let movesRemaining (48-movesUsed)
-//letWin
-//letLose
+//let win
+//let lose
 
 /*----- cached element references -----*/
 //firstCardPicked
 //secondCardPicked
-
 
 /*----- event listeners -----*/
 //playGame
 //flipCard
 //easyTimer
 //mediumTimer
-//difficultTimer
+//hardTimer
 //resetGame
 
 /*----- functions -----*/
@@ -32,5 +90,10 @@ console.log("Tayo-na!-Let's play!")
 //disableCard (when matched)
 //checkForMatch
 //unflipCard (when two unmacthed cards are flipped)
-//celebrateWinner (when all 12 pairs matched)
 //shuffleCards (activated at play game and reset)
+//timerEasy (icebox features)
+//timerMedium (icebox features)
+//timerHard(icebox features)
+//timesUp (icebox features)
+//noMoreTurns (when turns =48)
+//celebrateWinner (when all 12 pairs matched)
